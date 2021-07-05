7e441652fd1bfc25fec20211f818d07e19ce3d9d
marios-stam <mariosstamatopoulos1@gmail.com>
2021-07-05 17:44:11 +0300

    both hands and fist detection

--- hand_fingers_detection.py
+++ hand_fingers_detection.py
@@ -1,13 +1,29 @@
 import cv2
 import mediapipe as mp
+from utils import *
+
+FIST_DETECTED_COLOR=(255,0,0)
+LEFT_ORIGIN_X=20
+RIGHT_ORIGIN_X=450
+
+POINT_START_INDEX=0
+POINT_REF_INDEX=12
+
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 
 # For webcam input:
 cap = cv2.VideoCapture(0)
+
+def drawLandmarks(results):
+  for hand_landmarks in results.multi_hand_landmarks:
+    mp_drawing.draw_landmarks(
+        image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+
 with mp_hands.Hands(
     min_detection_confidence=0.5,
     min_tracking_confidence=0.5) as hands:
+  counter=0
   while cap.isOpened():
     success, image = cap.read()
     if not success:
@@ -27,11 +43,68 @@ with mp_hands.Hands(
     image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
     if results.multi_hand_landmarks:
+      drawLandmarks(results)
+      # for hand_landmarks in results.multi_hand_landmarks:
+      #   mp_drawing.draw_landmarks(
+      #       image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+      points,angles,distances,fistsDetected=[],[],[],[]
+
       for hand_landmarks in results.multi_hand_landmarks:
-        mp_drawing.draw_landmarks(
-            image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+        points.append(hand_landmarks.landmark)
+        
+        for i,p in enumerate(points[-1]):
+          points[-1][i].x,points[-1][i].y=convertPointCoordsToImagePlane(image,p)
+
+        angle=getAngle(points[-1][POINT_START_INDEX],points[-1][POINT_REF_INDEX])
+        distance=getDistance(points[-1][POINT_START_INDEX],points[-1][POINT_REF_INDEX])
+        fistDetect=fistDetected(points[-1])
+        
+        angles.append(angle)
+        distances.append(distance)
+        fistsDetected.append(fistDetect)
+        # image=drawPointToImage(image,points[0])
+        # image=drawPointToImage(image,points[12])
+
+        # txt="%.0f , %.0f" % (points[0].x,points[0].y )
+        # image=drawTextToImage(image,txt )
+        # txt="%.0f , %.0f" % (points[12].x,points[12].y )
+        # image=drawTextToImage(image,txt,origin=(20,60) )
+
+     
+    
+      if len(results.multi_hand_landmarks)>1:
+        txt="angle: %d" % (90-angles[1] )
+        image=drawTextToImage(image,txt,origin=(LEFT_ORIGIN_X,25))
+        txt="distance: %d" % (distances[1])
+        image=drawTextToImage(image,txt,origin=(LEFT_ORIGIN_X,45))
+      
+        txt="angle: %d" % (90-angles[0] )
+        image=drawTextToImage(image,txt,origin=(RIGHT_ORIGIN_X,25))
+        txt="distance: %d" % (distances[0])
+        image=drawTextToImage(image,txt,origin=(RIGHT_ORIGIN_X,45))
+        
+        if (fistsDetected[0]):
+          txt="FIST DETECTED"
+          image=drawTextToImage(image,txt,origin=(RIGHT_ORIGIN_X,70),color=FIST_DETECTED_COLOR )
+
+        if (fistsDetected[1]):
+          txt="FIST DETECTED"
+          image=drawTextToImage(image,txt,origin=(LEFT_ORIGIN_X,70),color=FIST_DETECTED_COLOR )
+      else:
+        txt="angle: %d" % (90-angles[0] )
+        image=drawTextToImage(image,txt,origin=(LEFT_ORIGIN_X,30))
+        txt="distance: %d" % (distances[0])
+        image=drawTextToImage(image,txt,origin=(LEFT_ORIGIN_X,50))
+        print()
+        if (fistsDetected[0]):
+          txt="FIST DETECTED"
+          image=drawTextToImage(image,txt,origin=(LEFT_ORIGIN_X,70),color=FIST_DETECTED_COLOR )
+        
     cv2.imshow('MediaPipe Hands', image)
-    input()
     if cv2.waitKey(5) & 0xFF == 27:
       break
+    counter=counter+1
+    if counter>100:
+      pass
+      #break
 cap.release()
---
+++ utils.py
@@ -0,0 +1,64 @@
+import math
+import cv2
+
+FIST_DISTANCE_THRESHOLD=100
+
+def getAngle(p1,p2):
+  dx=p1.x-p2.x
+  dy=p1.y-p2.y
+  
+  angle=math.atan2(dx,dy)
+  return  math.degrees(angle)
+
+def getDistance(p1,p2):
+    dx=p1.x-p2.x
+    dy=p1.y-p2.y
+    d=math.sqrt(dx**2+dy**2)
+    return d
+
+def drawTextToImage(image,text,origin=(20,30),color = (0, 0, 255)):
+    # font 
+    font = cv2.FONT_HERSHEY_COMPLEX_SMALL 
+
+    org=origin
+    # fontScale 
+    fontScale = 1
+    # convert RGB to BGR 
+    color= color[::-1]
+    # Line thickness of 2 px 
+    thickness = 2
+    # Using cv2.putText() method 
+    image = cv2.putText(image, text, org, font,  
+                       fontScale, color, thickness, cv2.LINE_AA) 
+    return image
+
+def drawPointToImage(image,point,radius=10):
+    x,y=point.x,point.y
+    x,y=int(x),int(y)
+    center_coordinates = (x,y)    
+    # Blue color in BGR
+    color = (0, 0, 255)
+    
+    # Line thickness of 2 px
+    thickness = 4
+    
+    # Using cv2.circle() method
+    # Draw a circle with blue line borders of thickness of 2 px
+    image = cv2.circle(image, center_coordinates, radius, color, thickness)
+    return image
+
+def convertPointCoordsToImagePlane(image,point):
+    height, width, channels = image.shape
+    x,y=point.x,point.y
+    
+    return x*width,y*height 
+
+def fistDetected(points):
+    if len(points)<15:
+        return False
+    p1=points[0]
+    p2=points[12]
+
+    d=getDistance(p1,p2)
+    print(d,(d<FIST_DISTANCE_THRESHOLD))
+    return (d<FIST_DISTANCE_THRESHOLD)
